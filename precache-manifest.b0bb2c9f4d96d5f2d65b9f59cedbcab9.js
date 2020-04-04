174822cd0c31753c240d1b369ab43fb930952308
Juan Carrera <juankarrera@gmail.com>
2020-04-04 15:14:03 -0400

    Updates

---
+++ precache-manifest.b0bb2c9f4d96d5f2d65b9f59cedbcab9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c411c617dca1160c644ea29551fba1ef",
+    "url": "/2020-04-04-Live-CSS-Editor/index.html"
+  },
+  {
+    "revision": "5f3c938fd58aa0c6d031",
+    "url": "/2020-04-04-Live-CSS-Editor/static/css/main.85a83d06.chunk.css"
+  },
+  {
+    "revision": "53c04687b271681d86fe",
+    "url": "/2020-04-04-Live-CSS-Editor/static/js/2.1c92a797.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2020-04-04-Live-CSS-Editor/static/js/2.1c92a797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f3c938fd58aa0c6d031",
+    "url": "/2020-04-04-Live-CSS-Editor/static/js/main.fc58c52d.chunk.js"
+  },
+  {
+    "revision": "422cc03ac6b28a88dfdf",
+    "url": "/2020-04-04-Live-CSS-Editor/static/js/runtime-main.a203642f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/2020-04-04-Live-CSS-Editor/static/media/logo.5d5d9eef.svg"
+  }
+]);
